99cbcdb7a14e84e8edb3e897975da86362676a2c
Lassi Kortela <lassi@lassi.io>
2023-06-03 17:08:23 +0300

    Fix stklos patch and example

--- impl-stklos/demo.scm
+++ impl-stklos/demo.scm
@@ -2,9 +2,12 @@
 
 (guard (err
         (unreadable-error?
-         (display "unreadable: ")
-         (write (unreadable-object-stand-in
-                 (unreadable-error-object err)))
-         (newline)))
+         (let* ((top (unreadable-error-object err))
+                (outer (unreadable-object-stand-in (list-ref top 5)))
+                (inner (unreadable-object-stand-in (list-ref outer 2))))
+           (define (wr msg obj) (display msg) (write obj) (newline))
+           (wr "inner stand-in: " inner)
+           (wr "outer stand-in: " outer)
+           (wr "top object: " top))))
 
        (read-from-string "(here is an unreadable object #[1 2 #[3 4 5]])"))
